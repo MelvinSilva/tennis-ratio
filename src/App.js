a9c603192e8f5df71b7b2f1ad7b567aefb74b06b
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-12-09 22:45:19 +0100

    new design

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
     return (
         <div>
             <h1 className='title-app'>🎾 TENNIS SERVICES RATIO 🎾</h1>
+            <p className='description'>Cette application permet de calculer le pourcentage de première balle réussie et de double faute effectué par rapport aux nombres total de services joués.<br/> Un tableau comparatif est ensuite généré pour classer les joueurs avec le ratio le plus élevé dans l'ordre décroissant.</p>
             <PlayerList />
         </div>
     );
